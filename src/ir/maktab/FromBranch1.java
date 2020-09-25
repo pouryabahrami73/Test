259d50f6fbbc60cd749d271c0874c49343b59ad3
Pourya Bahrami <pooria.bahrami73@gmail.com>
2020-09-25 23:47:17 +0330

    Add new line

--- src/ir/maktab/FromBranch1.java
+++ src/ir/maktab/FromBranch1.java
@@ -3,6 +3,6 @@ package ir.maktab;
 public class FromBranch1 {
     public static void main(String[] args) {
         System.out.println("from branch 1!!!");
-        System.out.println("is added in branch 2");
+        System.out.println("is added in branch 2 test");
     }
 }
