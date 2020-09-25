e12b2cbfc3f69bc458af8dc5923a68aea53e461c
Pourya Bahrami <pooria.bahrami73@gmail.com>
2020-09-25 23:40:36 +0330

    Add new line

--- src/ir/maktab/GitBranch1.java
+++ src/ir/maktab/GitBranch1.java
@@ -4,5 +4,6 @@ public class GitBranch1 {
     public static void main(String[] args) {
         System.out.println("from branch1");
         System.out.println("add in branch 2");
+        System.out.println("add from branch 2");
     }
 }
