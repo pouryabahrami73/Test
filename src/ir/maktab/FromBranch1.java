9ac0aaac9488137bf8ff6f6b1493b99807820e61
Pourya Bahrami <pooria.bahrami73@gmail.com>
2020-09-26 17:36:42 +0330

    Add new files

--- src/ir/maktab/FromBranch1.java
+++ src/ir/maktab/FromBranch1.java
@@ -3,5 +3,7 @@ package ir.maktab;
 public class FromBranch1 {
     public static void main(String[] args) {
         System.out.println("from branch 1!!!");
+        System.out.println("is added in branch 2 test");
+        System.out.println("salam for test push from branch 1");
     }
 }
--- src/ir/maktab/Main.java
+++ src/ir/maktab/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("salam akhavy");
         System.out.println("main: added form branch 1");
+        System.out.println("salam for test push from branch 2 in main");
     }
 }
