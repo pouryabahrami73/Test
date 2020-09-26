318ddfdc249bcafe5710e7195fa3cc010cbf7409
Pourya Bahrami <pooria.bahrami73@gmail.com>
2020-09-26 17:38:56 +0330

    Add new files

--- src/ir/maktab/GitBranch1.java
+++ src/ir/maktab/GitBranch1.java
@@ -3,5 +3,6 @@ package ir.maktab;
 public class GitBranch1 {
     public static void main(String[] args) {
         System.out.println("from branch1");
+        System.out.println("in gitbranch 1 from master");
     }
 }
